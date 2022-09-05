847f4740ddf2c2466130e0fa94263704d1abde37
masih <110786792+masihmo@users.noreply.github.com>
2022-09-05 20:20:47 +0430

    Add files via upload

---
+++ hotelservice.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract HotelService {
+
+    IERC20  mytoken = IERC20( 0xF2Ed42251F47fD00B382dd5AdF603426Bdafe8f9);
+    event SignUpEvent(address Owner,string indexed UserName, string indexed UserLastName, uint UserPrice);
+    event EnteredRome(address indexed  Owner);
+
+    struct Details {
+        string Name;
+        string LastName;
+        uint price;
+        uint Key;
+        uint Days;
+    }
+    
+    
+    Details[] Detail;
+    uint index;
+    uint Time;
+    address Owner;
+    bool SignUpBool;
+    bool PayBool;
+    bool ServiceStarted;
+    mapping  (address => Details) MapDetails;
+
+
+    modifier onlyOwner() {
+        require(msg.sender == Owner, "Not owner");
+        _;
+    }
+
+    function Exchange(address rec) public payable{
+        uint amount_of_tokens = msg.value / 10;
+        mytoken.transfer(rec , amount_of_tokens * 100);
+    }
+    
+    function SignUp(string memory _Name, string memory _LastName, uint _Days) public {
+        index++;
+        Detail.push(Details( _Name, _LastName, (_Days * 1) , uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))), _Days));
+        MapDetails[msg.sender].price = (_Days * 1 wei);
+        MapDetails[msg.sender].Key = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender)));
+        SignUpBool = true;
+        ServiceStarted = true;
+        Owner = msg.sender;
+        emit SignUpEvent(Owner ,_Name, _LastName, MapDetails[msg.sender].price);
+    }
+
+    function PayPrice() public view returns(uint) {
+        return MapDetails[msg.sender].price;
+    }
+
+    function Pay() public payable {
+        require(msg.value == MapDetails[msg.sender].price, "please enter correct value");
+        PayBool = true;
+        Time = block.timestamp;
+    }
+
+    function GetYourKey() public view returns(uint){
+        require(SignUpBool == true, "please signup first");
+        require(PayBool == true, "please pay first");
+        return MapDetails[msg.sender].Key;
+    }
+
+    function EnterYourRome(uint EnterYourKey) public onlyOwner {
+        require(EnterYourKey == MapDetails[msg.sender].Key , "Enter The Correct Key");
+        require(ServiceStarted == true, "");
+
+        if (block.timestamp > Time +(MapDetails[msg.sender].Days * 86400)) {
+            ServiceStarted = false;
+        }
+        emit EnteredRome(msg.sender);
+        //after this function called successfully you are going to see your room in front end
+    }
+    
+    function DaysRemain() public view onlyOwner returns(Details memory)  {
+        require(SignUpBool == true, "SignUp First");
+        return MapDetails[msg.sender];
+    }
+    
+ 
+}
